ae6f82b704fb600aa9ca14d16cdabc568abb36fd
+ <+>
2022-10-20 20:36:06 +0300

    commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import ContactForm from './components/ContactForm'
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  )
+}
+export default App
