4e0d7a5796f07230d07028db78d6a3cd721d8593
korosdennis <deniptoo98@gmaiil.com>
2022-10-21 16:24:17 +0300

    models

--- db/migrate/20221020083448_create_parcels.rb
+++ db/migrate/20221020083448_create_parcels.rb
@@ -1,6 +1,9 @@
 class CreateParcels < ActiveRecord::Migration[7.0]
   def change
     create_table :parcels do |t|
+
+      t.integer :user_id
+      t.integer :parcel_category_id
       t.string :description
       t.string :recipient_name
       t.integer :recipient_contact
--- db/migrate/20221020083849_create_deliveries.rb
+++ db/migrate/20221020083849_create_deliveries.rb
@@ -3,6 +3,8 @@ class CreateDeliveries < ActiveRecord::Migration[7.0]
     create_table :deliveries do |t|
       t.string :recipient_name
       t.timestamp :reception_time
+      t.integer :order_id
+      t.integer :bill_id
 
       t.timestamps
     end
