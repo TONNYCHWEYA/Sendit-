1d4cf954d219da425f8adbc16f0f7c81acc4f4f3
+ <+>
2022-10-24 12:48:30 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import ContactForm from './components/ContactForm'
-import AboutUs from './components/AboutUs'
-import MyAccount from './components/MyAccount'
-
 
 function App() {
   return (
     <div className="App">
-      <ContactForm />
-      <AboutUs/>
-
+      hello
     </div>
-  )
+  );
 }
-export default App
+
+export default App;
