9c0076cc334d866c5c458b52d24547558ea93e76
TONNYCHWEYA <chweyatonny@gmail.com>
2022-10-24 07:37:34 +0300

    Added React router dom

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ import Login from './components/Login';
 import React from 'react';
 import Navb from './components/Navb';
 import Signup from './components/Signup';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 
