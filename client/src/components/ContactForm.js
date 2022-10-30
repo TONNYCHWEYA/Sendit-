309958efcd9a0a8e871636e4575ef8aabbfb167d
+ <+>
2022-10-30 17:33:46 +0300

    post request for contact form

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Card,Button } from 'react-bootstrap'
 
-
-const ContactForm = () => {
-  const [formStatus, setFormStatus] = React.useState('Send')
-  const onSubmit = (e) => {
-    e.preventDefault()
-    setFormStatus('Submitting...')
-    const { FirstName, LastName, email, message } = e.target.elements
-    let conFom = {
-      FirstName: FirstName.value,
-      LasName: LastName.value,
-      email: email.value,
-      message: message.value,
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      FirstName: '',
+      Lastname: '',
+      email: '',
+      message: ''
     }
-    console.log(conFom)
   }
-  return (
-    <div className="form-group">
-      <Card style={{background: '#1A237E', color: 'white',margin:'20px'}}>
+  handleSubmit(e){
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url:"",
+      data:  this.state
+    }).then((response)=>{
+      if (response.data.status === 'success') {
+        alert("Message Sent.");
+        this.resetForm()
+      } else if (response.data.status === 'fail') {
+        alert("Message failed to send.")
+      }
+    })
+  }
+  resetForm(){
+    this.setState({FirstName: '',Lastname: '', email: '', message: ''})
+  }
+  render() {
+    return(
+      <div className="App">
+        <Card style={{background: '#1A237E', color: 'white',margin:'20px'}}>
         <Card.Body>
           <Card.Title>
             <h3>Contact Us</h3>
@@ -27,45 +42,44 @@ const ContactForm = () => {
         </Card.Body>
       </Card>
 
-      <Card style={{background: '#E8EAF6'}}>
-      <form className='ma' onSubmit={onSubmit}>
-          <div class="form-group">
-            <div className="mb-3">
-              <label className="form-label" htmlFor="first name">
-                First name
-              </label>
-              <input className="form-control" type="text" id="first name" required />
-            </div>
-
-            <div className="mb-3">
-              <label className="form-label" htmlFor="lastname">
-                Last name 
-              </label>
-              <input className="form-control" type="text" id="last name" required />
-            </div>
-
-            <div className="mb-3">
-              <label className="form-label" htmlFor="email">
-                Email
-              </label>
-              <input className="form-control" type="email" id="email" required />
-            </div>
+      <Card style={{background: '#E8EAF6', margin:'20px'}}>
+        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+          <div className="form-group">
+              <label htmlFor="Firstname">First Name</label>
+              <input type="text" className="form-control" id="name" value={this.state.FirstName} onChange={this.onFirstNameChange.bind(this)} required/>
           </div>
-
-        <div className='form-group'>
-          <div className="mb-3" >
-            <label className="form-label" htmlFor="message">
-              Message
-            </label>
-            <textarea className="form-control" id="message" required />
+          <div className="form-group">
+              <label htmlFor="LastName">Last Name</label>
+              <input type="text" className="form-control" id="Last Name" value={this.state.LastName} onChange={this.onLastNameChange.bind(this)} required/>
           </div>
-        </div>
-        <Button  type="submit" style={{background: '#1A237E', color: 'white'}}>
-          {formStatus}
-        </Button>
-      </form>
-      </Card>
-    </div>
-  )
+          <div className="form-group">
+              <label htmlFor="email">Email address</label>
+              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} required/>
+          </div>
+          <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} required/>
+          </div>
+          
+          <Button type="submit" style={{background: '#1A237E', color: 'white'}}>
+            Submit
+          </Button>
+        </form>
+        </Card>
+      </div>
+    );
+  }
+  onFirstNameChange(event) {
+	  this.setState({FirstName: event.target.value})
+  }
+  onLastNameChange(event) {
+	  this.setState({LastName: event.target.value})
+  }
+  onEmailChange(event) {
+	  this.setState({email: event.target.value})
+  }
+  onMessageChange(event) {
+	  this.setState({message: event.target.value})
+  }
 }
-export default ContactForm
+export default App;
--- client/src/components/LatestOrders.js
+++ client/src/components/LatestOrders.js
@@ -1,12 +1,24 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import "../App.css"
 import {Table, Card } from 'react-bootstrap'
-import data from '../mock-data.json'
 
+//template for making the get request
 const LatestOrders = () => {
-    const [orders] = useState(data);
+    const [orders, setOrders] = useState([]);
+        useEffect(() => {
+        fetch('')
+           .then((response) => response.json())
+           .then((data) => {
+              console.log(data);
+              setOrders(data);
+           })
+           .catch((err) => {
+              console.log(err.message);
+           });
+        }, []);
     return(
         <div className="app-container">
+            
             <Card style={{background: '#1A237E', color: 'white', margin:'20px'}}>
                 <Card.Body>
                 <Card.Title>
@@ -14,31 +26,34 @@ const LatestOrders = () => {
                 </Card.Title>
                 </Card.Body>
             </Card>
-            <Table striped bordered hover size="sm">
-                <thead>
-                    <tr>
-                        <th>Order ID</th>
-                        <th>Customer Name</th>
-                        <th>From</th>
-                        <th>To</th>
-                        <th>Collecting date</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {orders.map((order)=> (
-                    <tr>
-                        <td>{order.order_id}</td>
-                        <td>{order.customername}</td>
-                        <td>{order.from}</td>
-                        <td>{order.to}</td>
-                        <td>{order.collectingdate}</td>
-                        <td>{order.status}</td>
-                    </tr>
-                    ))}
-                </tbody>
-            </Table>
-
+            <div className='latest-orders'>
+                {orders.map((order) => {
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>Order ID</th>
+                            <th>Customer Name</th>
+                            <th>From</th>
+                            <th>To</th>
+                            <th>Collecting date</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((order)=> (
+                        <tr>
+                            <td>{order.order_id}</td>
+                            <td>{order.customername}</td>
+                            <td>{order.from}</td>
+                            <td>{order.to}</td>
+                            <td>{order.collectingdate}</td>
+                            <td>{order.status}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            })}
+            </div>
         </div>
     )
 }
