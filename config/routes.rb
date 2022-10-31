abeca4f510e8250d1beb6791935e7261d9e5c8b9
korosdennis <deniptoo98@gmaiil.com>
2022-10-31 23:16:59 +0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'location_assigned/parcel_carrier_id:integer'
-  get 'location_assigned/location_id:integer'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
 end
