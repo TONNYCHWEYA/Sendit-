83170d593fca3be1bd26dd05fabdd82e9393bb74
korosdennis <deniptoo98@gmaiil.com>
2022-11-01 11:19:56 +0300

    new branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(first_name:"dennis",last_name:"kiptoo",ema
+    il_address:"dennismail.com",phone_number:"1234512",username:"dennis
+    ",password:"koros123")
