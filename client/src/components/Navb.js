6c6b7368633ba2688ea314f69d51dfdc9eadb026
+ <+>
2022-11-08 15:48:44 +0300

    commit

--- client/src/components/Navb.js
+++ client/src/components/Navb.js
@@ -1,45 +1,73 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import {
- Link
-  
- } from "react-router-dom";
- import React from 'react';
- import styled from "styled-components"
- const Style = styled.div`
- .nav{
-  background-color: #1A237E;
-  text-color: white;
- }
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import React from "react";
+import styled from "styled-components";
+const Style = styled.div`
+  .nav {
+    background-color: #1a237e;
+    text-color: white;
+  }
 `;
 
-function Navb({setUser, user}) {
-  const handleLogout = () =>{
-    setUser(null)
-    localStorage.removeItem("token")
-  }
+function Navb({ setUser, user }) {
+  const handleLogout = () => {
+    setUser(null);
+    localStorage.removeItem("token");
+  };
   return (
     <Style>
-    <Navbar expand="lg" className='nav' style={{background: '#1A237E', color: 'white'}}>
-      <Container>
-        <Navbar.Brand href="#home">SENDIT</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
-           
-            <Nav.Link as={Link} to={"/sendaparcel"}>Send a parcel</Nav.Link>
-            <Nav.Link as={Link} to={"/about"}>About Us</Nav.Link>
-            <Nav.Link as={Link} to={"/contactus"}>Contact Us</Nav.Link>
-            <Nav.Link as={Link} to={"/myaccount"}>My account</Nav.Link>
-            {user ?   <Nav.Link as={Link} to={"/login"} onClick={handleLogout}>Logout</Nav.Link> : <Nav.Link as={Link} to={"/login"}>Login</Nav.Link>}
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar
+        expand="lg"
+        className="nav"
+        style={{ background: "#1A237E", color: "white" }}
+      >
+        <Container>
+          <Navbar.Brand style={{ color: "white" }} href="#home">
+            SENDIT
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link style={{ color: "white" }} as={Link} to={"/"}>
+                Home
+              </Nav.Link>
+
+              <Nav.Link
+                style={{ color: "white" }}
+                as={Link}
+                to={"/sendaparcel"}
+              >
+                Send a parcel
+              </Nav.Link>
+              <Nav.Link style={{ color: "white" }} as={Link} to={"/about"}>
+                About Us
+              </Nav.Link>
+              <Nav.Link style={{ color: "white" }} as={Link} to={"/contactus"}>
+                Contact Us
+              </Nav.Link>
+              <Nav.Link style={{ color: "white" }} as={Link} to={"/myaccount"}>
+                My account
+              </Nav.Link>
+              {user ? (
+                <Nav.Link
+                  as={Link}
+                  style={{ color: "white" }}
+                  to={"/login"}
+                  onClick={handleLogout}
+                >
+                  Logout
+                </Nav.Link>
+              ) : (
+                <Nav.Link style={{color:'white'}} as={Link} to={"/login"}>
+                  Login
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </Style>
   );
 }
